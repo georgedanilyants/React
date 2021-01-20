24af7b047a63533aac58514d8fc1dbc327f9d4e0
George Danilyants <sax0691@gmail.com>
2021-01-21 02:18:08 +0300

    fix: устранить проблему с подписью авторов сообщений

---
+++ static_src/components/app.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState, useCallback} from "react";
+
+import Message from "./message";
+import Input from "./input";
+
+export default function App() {
+    const [messages, setMessages] = useState([
+        {text: "message1", author: "me"},
+        {text: "message2", author: "me"},
+    ]);
+
+    const renderMessage = useCallback((message, i) => {
+        return <Message message={message} key={i} />;
+    }, []);
+
+    const handleAddMessage = useCallback((text, author = 'me') => {
+        setMessages((oldMessages) => ([...oldMessages, { text, author }]));
+    }, []);
+
+    useEffect(() => {
+        let timeout;
+        if (messages[messages.length - 1].author !== 'robot') {
+            const timeout = setTimeout(() => {
+                handleAddMessage('some answer', 'robot')
+            }, 1500)
+        }
+
+        return () => {
+            clearTimeout(timeout);
+        }
+    }, [messages, handleAddMessage]);
+    
+    return (
+        <>
+            {messages.map(renderMessage)}
+            <Input onAddMessage={handleAddMessage} />
+        </>
+    );
+}
